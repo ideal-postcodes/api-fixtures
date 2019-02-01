1c568725a45d8362a9ad0537ddd1c0ea23285090
Chris Blanchard <cablanchard@gmail.com>
2019-02-01 16:56:06 +0100

    Spike

---
+++ bin/generate.ts
@@ -0,0 +1,22 @@
+import { writeFileSync } from "fs";
+import { join } from "path";
+
+// const resources = require("../dist/index");
+// import { PostcodesResource } = resources;
+
+// const httpAgent = {
+//   request: options => {
+//   }
+// };
+
+// const write = ({ content, fileName }) => {
+//   const filePath = join("./", fileName);
+//   const encoding = "utf8";
+//   writeFileSync(filePath, content, { encoding });
+// };
+
+// PostcodesResource.generateFixtures({ httpAgent })
+//   .then(fixtures => fixtures.forEach(write))
+//   .catch(error => {
+//     console.log(error);
+//   });
--- lib/fixtures/postcodes.ts
+++ lib/fixtures/postcodes.ts
@@ -1,6 +1,7 @@
 import { payload as success } from "./postcodes/2000-postcodes";
 import { payload as notFound } from "./postcodes/4040-postcodes";
-import { payload as mrSuccess } from "./postcodes/2000-postcodes";
+import { payload as mrSuccess } from "./postcodes/2000-postcodes-mr";
+import { Definition } from "../resource";
 
 export const postcodes = {
   success,
@@ -9,4 +10,15 @@ export const postcodes = {
     success: mrSuccess,
   },
 };
+
+export const definitions: Definition[] = [
+  {
+    name: "2000-postcodes",
+    description: "Successful postcode resolution request",
+    url: "/v1/postcodes/SW1A2AA",
+    query: {
+      api_key: "<VALID_API_KEY>"
+    },
+  },
+];
 
