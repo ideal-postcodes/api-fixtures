a0a05ce32b19431e3ef1b6d1413589353b3504c2
Chris Blanchard <cablanchard@gmail.com>
2019-02-06 16:21:33 +0100

    Verify httpAgent: - Makes post request with json body - Can override method and headers

--- tests/http_agent.unit.ts
+++ tests/http_agent.unit.ts
@@ -14,27 +14,52 @@ describe("httpAgent", () => {
     nock.enableNetConnect();
   });
 
-  const result = {
-    result: "Thanks for trying ideal-postcodes.co.uk",
-    code: 2000,
-    message: "Success",
-  };
 
-  const createInterceptor = () => {
-    return nock("https://api.ideal-postcodes.co.uk")
+  it("dispatches http request with right defaults", async () => {
+    const url = "/";
+    const result = {
+      result: "Thanks for trying ideal-postcodes.co.uk",
+      code: 2000,
+      message: "Success",
+    };
+    const query = { foo: "bar" };
+    const scope = nock("https://api.ideal-postcodes.co.uk")
       .get("/")
       .query({ foo: "bar" })
       .reply(HTTP_SUCCESS, result);
-  };
-
-  it("dispatches http request with right defaults", async () => {
-    const scope = createInterceptor();
-    const { body, httpStatus } = await httpAgent({
-      url: "/",
-      query: { foo: "bar" },
-    });
+    const { body, httpStatus } = await httpAgent({ url, query });
     assert.equal(httpStatus, HTTP_SUCCESS);
     assert.deepEqual(body, result);
     assert.isTrue(scope.isDone());
   });
+
+  it ("dispatches POST request with body", async () => {
+    const url = "/";
+    const requestBody = { "foo": "bar" };
+    const method = "POST";
+    const scope = nock("https://api.ideal-postcodes.co.uk")
+      .post("/", requestBody)
+      .reply(HTTP_SUCCESS, {});
+    const { body, httpStatus } = await httpAgent({ url, body: requestBody, method });
+    assert.equal(httpStatus, HTTP_SUCCESS);
+    assert.deepEqual(body, {});
+    assert.isTrue(scope.isDone());
+  });
+
+  it ("allows header, method to be overriden", async () => {
+    const url = "/";
+    const headers = {
+      "Content-Type": "application/xml",
+      "Foo": "Bar",
+    };
+    const method = "HEAD";
+    const scope = nock("https://api.ideal-postcodes.co.uk", { reqheaders: headers })
+      .head("/")
+      .reply(HTTP_SUCCESS, {});
+    const { body, httpStatus } = await httpAgent({ url, method, headers });
+    assert.equal(httpStatus, HTTP_SUCCESS);
+    assert.deepEqual(body, {});
+    assert.isTrue(scope.isDone());
+  });
 });
+
