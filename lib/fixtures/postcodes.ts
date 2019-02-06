24b711343384146f321967ff80e57e4013daf00e
Chris Blanchard <cablanchard@gmail.com>
2019-02-06 16:24:52 +0100

    Fix 2000 MR export

--- lib/fixtures/postcodes.ts
+++ lib/fixtures/postcodes.ts
@@ -1,6 +1,6 @@
 import { payload as success } from "./postcodes/2000-postcodes";
 import { payload as notFound } from "./postcodes/4040-postcodes";
-import { payload as mrSuccess } from "./postcodes/2000-postcodes";
+import { payload as mrSuccess } from "./postcodes/2000-postcodes-mr";
 
 export const postcodes = {
   success,
