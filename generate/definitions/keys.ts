38f1f8ef80c1ffe38694d8d82e5c82a0e91ce562
Christopher Blanchard <cablanchard@gmail.com>
2019-06-04 09:48:51 +0200

    feat(Keys): Add definitions
    - Add definition for key usage
    - Add definition for private key info

--- generate/definitions/keys.ts
+++ generate/definitions/keys.ts
@@ -16,11 +16,28 @@ const keys: Definition[] = [
     headers: {},
   },
   {
-
     name: "2000-keys-unavailable",
     description: "Unavailable API Key",
     url: "/v1/keys/idkfa",
     query: {},
+    headers: {},
+  },
+  {
+    name: "2000-keys-usage",
+    description: "API key recent usage statistics",
+    url: "/v1/keys/<VALID_API_KEY>/usage",
+    query: {
+      user_token: "<USER_TOKEN>"
+    },
+    headers: {},
+  },
+  {
+    name: "2000-keys-private",
+    description: "API Key query for private information",
+    url: "/v1/keys/<VALID_API_KEY>",
+    query: {
+      user_token: "<USER_TOKEN>"
+    },
     headers: {},
   },
 ];
